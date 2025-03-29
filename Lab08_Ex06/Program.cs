695adba12a71698ef204d01b703ab32383bc7c76
samaj43981 <97647605+samaj43981@users.noreply.github.com>
2025-03-30 05:06:30 +0700

    klg

---
+++ Lab08_Ex06/Program.cs
@@ -0,0 +1,18 @@
+﻿MyClass mc = new MyClass();
+mc.CheckEvenOdd(5);
+mc.CheckEvenOdd(10);
+
+public class MyClass
+{
+    public void CheckEvenOdd(int number)
+    {
+        if (number % 2 == 0)
+        {
+            System.Console.WriteLine($"{number} เป็นเลขคู่");
+        }
+        else
+        {
+            System.Console.WriteLine($"{number} เป็นเลขคี่");
+        }
+    }
+}
---
+++ Lab08_Ex07/Program.cs
@@ -0,0 +1,35 @@
+﻿public class MyProgram
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public void GenerateMultiplicationTables(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            System.Console.WriteLine($"สูตรคูณแม่ {i}:");
+            for (int j = 1; j <= 12; j++)
+            {
+                int result = multiply(i, j);
+                System.Console.WriteLine($"{i} x {j} = {result}");
+            }
+            System.Console.WriteLine(); // เพิ่มบรรทัดว่างเพื่อคั่นระหว่างแม่สูตรคูณ
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyProgram myProgram = new MyProgram();
+        myProgram.GenerateMultiplicationTables(2, 12); // สร้างสูตรคูณแม่ 2-12
+    }
+}
